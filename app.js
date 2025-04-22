3d7c0daea12d06a1c0da2d385c0a2a90559be8c0
Yash Dadhich <yashdadhichajmer@gmail.com>
2025-04-22 21:22:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+const boxes = document.querySelectorAll(".box");
+const resetBtn = document.querySelector("#reset-btn");
+let turnO = true; // Represents if it's Player O's turn (otherwise it's Player X)
+let count = 0; // To track the number of moves for a potential draw
+
+const winningPatterns = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const disableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+
+const showWinner = (winner) => {
+  alert(`Congratulations, Winner is ${winner}`);
+  disableBoxes();
+};
+
+const checkWinner = () => {
+  for (let pattern of winningPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        showWinner(pos1Val);
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO) {
+      box.innerText = "O";
+      turnO = false;
+    } else {
+      box.innerText = "X";
+      turnO = true;
+    }
+    box.disabled = true;
+    count++;
+
+    let isWinner = checkWinner();
+
+    if (count === 9 && !isWinner) {
+      alert("Game Draw!");
+    }
+  });
+});
+
+resetBtn.addEventListener("click", enableBoxes);
